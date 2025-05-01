4638872f24de117f2a6818cdaefe415b766532b8
Leopoldo Araujo <68853890+poloaraujo@users.noreply.github.com>
2025-05-01 10:19:15 -0400

    Added certifi

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(name="pipelinewise-target-snowflake",
           'inflection==0.5.1',
           'joblib==1.2.0',
           'boto3==1.28.20',
+          'certifi==2025.01.31',
       ],
       extras_require={
           "test": [
